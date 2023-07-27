374bb2e5ecbe89a087f48c9e60a577b0b746c331
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-07-27 13:52:33 -0700

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,17 @@
-type ExtractMethods<T> = {
-    [K in keyof T as T[K] extends Function ? K : never]: T[K]
-};
-
-type Mutable<T> = {
-    -readonly [P in keyof T]: T[P];
-};
+type Function = (...args: unknown[]) => Promise<unknown> | unknown;
 
 type Prettify<T> = {
     [K in keyof T]: T[K];
 } & {};
 
+type SyncFunction<T, R = T extends Function ? ReturnType<T> : T> =
+    T extends Promise<unknown>
+        ? never
+        : T extends Function
+            ? SyncFunction<R> extends never
+                ? never
+                : T
+            : T;
 
-export { ExtractMethods, Mutable, Prettify };
+
+export { Prettify, SyncFunction };
