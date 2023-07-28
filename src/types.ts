824e0b91a7484c03607de919d116dde72c8b44b4
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-07-28 02:54:37 -0700

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 type Function = (...args: unknown[]) => Promise<unknown> | unknown;
 
-type Prettify<T> = {
-    [K in keyof T]: T[K];
-} & {};
-
-type SyncFunction<T, R = T extends ((...args: unknown[]) => Promise<unknown> | unknown) ? ReturnType<T> : T> =
+type NeverAsync<T> =
     T extends Promise<unknown>
         ? never
         : T extends ((...args: unknown[]) => Promise<unknown> | unknown)
-            ? SyncFunction<R> extends never
+            ? NeverAsync<ReturnType<T>> extends never
                 ? never
                 : T
             : T;
 
+type Prettify<T> = {
+    [K in keyof T]: T[K];
+} & {};
 
-export { Function, Prettify, SyncFunction };
+
+export { Function, NeverAsync, Prettify };
